326623206c70114b86a1f35808ad49cb83de5979
iaish-max <aishwarybhadauriya09@gmail.com>
2022-02-24 19:53:31 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Quiz from "./Components/Quiz/Quiz";
+import OTPModal from "./Components/OTPModal/OTPModal";
+import { useEffect, useState } from "react";
+import AddQuestion from "./Components/addQuestion/AddQuestion";
+import Cookies from "js-cookie";
 
 function App() {
+  const [loginDetails, setLoginDetails] = useState({ password: "", email: "" });
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [userSignUP, setUserSignUP] = useState(false);
+
+  useEffect(() => {
+    if (Cookies.get("refreshToken")) {
+      setUserLoggedIn(true);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <Home
+                setLoginDetails={setLoginDetails}
+                loginDetails={loginDetails}
+                userLoggedIn={userLoggedIn}
+                setUserLoggedIn={setUserLoggedIn}
+                userSignUP={userSignUP}
+              />
+            }
+          ></Route>
+          <Route path="/:CategoryName/:CategoryId" element={<Quiz />}></Route>
+          <Route
+            path="/email-activation"
+            element={
+              <OTPModal
+                loginDetails={loginDetails}
+                setLoginDetails={setLoginDetails}
+                setUserSignUP={setUserSignUP}
+              />
+            }
+          ></Route>
+          <Route path="/addQuestion" element={<AddQuestion />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
